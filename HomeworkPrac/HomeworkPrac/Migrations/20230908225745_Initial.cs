fb6efdf4b736ba9f0279ace507eedac5063aa919
Pk Dagada <36564567@mynwu.ac.za>
2023-09-08 23:58:32 +0100

    migration

---
+++ HomeworkPrac/HomeworkPrac/Migrations/20230908225745_Initial.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeworkPrac.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    ProductID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductName = table.Column<string>(type: "varchar(max)", unicode: false, nullable: true),
+                    ProductDescription = table.Column<string>(type: "varchar(max)", unicode: false, nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ProductID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Service",
+                columns: table => new
+                {
+                    ServiceID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ServiceName = table.Column<string>(type: "varchar(max)", unicode: false, nullable: false),
+                    ServiceDescription = table.Column<string>(type: "varchar(max)", unicode: false, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Service", x => x.ServiceID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "Service");
+        }
+    }
+}
